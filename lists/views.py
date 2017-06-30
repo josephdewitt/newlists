578ee73d8c0bdb1ec63460f429c23491ac0a7b0b
Joseph P DeWitt <jpdewitt@jpsenior.com>
2017-06-30 17:04:30 -0500

    emforce model validation in list view with mt hack

--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
+
 def home_page(request):
     return render(request, 'home.html')
 
@@ -18,13 +19,18 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
+    error = None
 
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+#            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
