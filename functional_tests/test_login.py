48e783cb68ce8d3933d07cef1849cd2c0cba5898
Joseph P DeWitt <jpdewitt@jpsenior.com>
2017-08-27 17:11:28 -0500

    put in @contextmanager

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -4,6 +4,7 @@ import re
 import os
 import poplib
 import time
+from contextlib import contextmanager
 
 from .base import FunctionalTest
 
@@ -55,33 +56,68 @@ class LoginTest(FunctionalTest):
         # She is logged out
         self.wait_to_be_logged_out(email=test_email)
 
+    # def wait_for_email(self, test_email, subject):
+    #     if not self.staging_server:
+    #         email = mail.outbox[0]
+    #         self.assertIn(test_email, email.to)
+    #         self.assertEqual(email.subject, subject)
+    #         return email.body
+    #
+    #     email_id = None
+    #     start = time.time()
+    #     inbox = poplib.POP3_SSL('mail.jpsenior.com')
+    #     try:
+    #         inbox.user('joseph')
+    #         inbox.pass_(os.environ['YAHOO_PASSWORD'])
+    #
+    #         while time.time() - start < 60:
+    #             get 10 newest messages
+                # count, _ = inbox.stat()
+                # for i in reversed(range(max(1, count - 10), count + 1)):
+                #     print('getting msg', i)
+                #     _, lines, __ = inbox.retr(i)
+                #     lines = [l.decode('utf8') for l in lines]
+                #     print(lines)
+                #     if 'Subject: %s' % subject in lines:
+                #         email_id = i
+                #         body = '\n'.join(lines)
+                #         return body
+                # time.sleep(5)
+        # finally:
+        #     if email_id:
+        #         inbox.dele(email_id)
+        #     inbox.quit()
+
+    @contextmanager
+    def pop_inbox(self):
+        try:
+            inbox = poplib.POP3_SSL('mail.jpsenior.com')
+            inbox.user('joseph')
+            inbox.pass_(os.environ['YAHOO_PASSWORD'])
+            yield inbox
+
+        finally:
+            inbox.quit()
+
     def wait_for_email(self, test_email, subject):
         if not self.staging_server:
             email = mail.outbox[0]
             self.assertIn(test_email, email.to)
-            self.assertEqual(email.subject, subject)
+            self.assertEqual(subject, email.subject)
             return email.body
 
-        email_id = None
+        last_count = 0
         start = time.time()
-        inbox = poplib.POP3_SSL('mail.jpsenior.com')
-        try:
-            inbox.user('joseph')
-            inbox.pass_(os.environ['YAHOO_PASSWORD'])
-            while time.time() - start < 60:
-                # get 10 newest messages
+
+        while time.time() - start < 60:
+            with self.pop_inbox() as inbox:
                 count, _ = inbox.stat()
-                for i in reversed(range(max(1, count - 10), count + 1)):
-                    print('getting msg', i)
-                    _, lines, __ = inbox.retr(i)
-                    lines = [l.decode('utf8') for l in lines]
-                    print(lines)
-                    if 'Subject: %s' % subject in lines:
-                        email_id = i
-                        body = '\n'.join(lines)
-                        return body
-                time.sleep(5)
-        finally:
-            if email_id:
-                inbox.dele(email_id)
-            inbox.quit()
+                if count != last_count:
+                    for i in range(count, last_count, -1):
+                        _, lines, __ = inbox.retr(i)
+                        lines = [l.decode('utf8') for l in lines]
+                        if 'Subject: %s'  % subject in lines:
+                            inbox.dele(i)
+                            return '\n'.join(lines)
+                    last_count = count
+            time.sleep(5)
